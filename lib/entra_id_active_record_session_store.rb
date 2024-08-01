ddecd796b880345e37bddc4078a1cfeb3cd868a6
David Susco <dsusco@clarku.edu>
2024-08-01 09:00:22 -0400

    initial commit

---
+++ lib/entra_id_active_record_session_store.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "entra_id_active_record_session_store/version"
+
+module EntraIdActiveRecordSessionStore
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "entra_id_active_record_session_store"
+
+require "minitest/autorun"
