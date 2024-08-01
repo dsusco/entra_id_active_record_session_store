fba511a6bbb217270de618a454e50f95d467e0ab
David Susco <dsusco@clarku.edu>
2024-08-01 13:17:29 -0400

    added ActiveRecordSessionStore class and fixed initializer generator typo

--- lib/entra_id_active_record_session_store.rb
+++ lib/entra_id_active_record_session_store.rb
@@ -1,4 +1,5 @@
 require 'rack_entra_id_auth'
+require 'action_dispatch/session/entra_id_active_record_store'
 
 module EntraIdActiveRecordSessionStore
 end
--- lib/generators/entra_id_active_record_session_store/templates/initializer.rb
+++ lib/generators/entra_id_active_record_session_store/templates/initializer.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :entra_id_active_record_store, :key => "_#{File.basename(Rails.root)}_session"
+Rails.application.config.session_store :entra_id_active_record_store, :key => '_<%= File.basename(Rails.root) %>_session'
